69054a2deca6f86051fd8b441c3929a2fe206b23
amit <amitsr4@gmail.com>
2023-01-12 14:13:23 +0200

    first commit!

---
+++ singlelinkage.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy.io as sio
+
+
+def singlelinkage(X, k):
+    """
+    :param X: numpy array of size (m, d) containing the test samples
+    :param k: the number of clusters
+    :return: a column vector of length m, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned.
+    """
+    raise NotImplementedError()
+
+
+def simple_test():
+    # load sample data (this is just an example code, don't forget the other part)
+    data = np.load('mnist_all.npz')
+    X = np.concatenate((data['train0'], data['train1']))
+    m, d = X.shape
+
+    # run single-linkage
+    c = singlelinkage(X, k=10)
+
+    assert isinstance(c, np.ndarray), "The output of the function softsvm should be a numpy array"
+    assert c.shape[0] == m and c.shape[1] == 1, f"The shape of the output should be ({m}, 1)"
+
+
+if __name__ == '__main__':
+    # before submitting, make sure that the function simple_test runs without errors
+    simple_test()
+
+    # here you may add any code that uses the above functions to solve question 2
